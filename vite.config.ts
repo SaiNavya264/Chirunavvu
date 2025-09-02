30902ca613ad8bf14862717b98f16c778f5bfd05
SaiNavya264 <sainavya264@gmail.com>
2025-09-02 17:22:28 +0530

    Add homepage and base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Chirunavvu/",   // 👈 matches your repo name exactly
+  base: '/Chirunavvu/'   // 👈 must match your repo name
 })
